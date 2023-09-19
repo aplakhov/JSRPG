834bc15df21c9bf72ac41b905f0bd02f54555798
finder <aplakhovprivate@gmail.com>
2023-09-19 23:47:36 +0300

    work in progress for first northern map

--- north_map_script.js
+++ north_map_script.js
@@ -1,6 +1,31 @@
+characters.northern_woman = {
+    text: "Бедная женщина", 
+    addText: "Как они тут вообще живут?", 
+    portrait: "Dialogs/northern_woman"
+};
+
+quests.nothern_main = {
+    map: "north_map",
+    text: "Починить манапровод",
+    place: "house",
+    character: "northern_woman",
+    introDialog: [
+        ["", "Заглушка", []], // 0
+    ],
+}
+
 class NorthMapScript extends AllScripts {
     constructor(world) {
         super();
+
+        let house = world.scriptObjects.house;
+        house.inside = {
+            art: "Places/northern_house",
+            header: 'Домик из брёвен',
+            description: 
+                'Заглушка.',
+        }
+
         for (let x = 0; x < world.width; x++) {
             for (let y = 0; y < world.height; y++) {
                 if (world.terrain[x][y] == TERRAIN_STONE_WALL) {
@@ -12,7 +37,6 @@ class NorthMapScript extends AllScripts {
                         "class": "DecorativeObject",
                         "zLayer": 1,
                         "name": "Манапровод",
-                        "Blocker": true 
                     };
                     if (leftToo && rightToo && upToo && downToo)
                         o["Image"] = "Pipe/pipe-x";
@@ -36,6 +60,29 @@ class NorthMapScript extends AllScripts {
             for (let y = 0; y < world.height; y++)
                 if (world.terrain[x][y] == TERRAIN_STONE_WALL)
                     world.terrain[x][y] = TERRAIN_GRASS;
+        const brokenPipeContent = ['lulux', 'ululu', 'luuux', 'uxuxu', 'llull'];
+        const brokenPipe = {x: 12, y: 32} //world.scriptObjects.brokenPipe;
+        for (let x = 0; x < 5; x++) {
+            for (let y = 0; y < 5; y++) {
+                const pipeType = brokenPipeContent[y][x];
+                let img;
+                if (pipeType == 'l')
+                    img = randomFrom(["Pipe/pipe-v", "Pipe/pipe-h"]);
+                else if (pipeType == 'u')
+                    img = randomFrom(["Pipe/pipe-ul", "Pipe/pipe-ur", "Pipe/pipe-dl", "Pipe/pipe-dr"]);
+                else
+                    img = "Pipe/pipe-x";
+                let o = {
+                    "class": "DecorativeObject",
+                    "zLayer": 1,
+                    "name": "Разрушенный манапровод",
+                    "Image": img 
+                };
+                let added = world.addNewObject(o, x + brokenPipe.x, y + brokenPipe.y)
+                if (pipeType != 'x')
+                    added.onContact = () => { _rotatePipe(added); } 
+            }
+        }
     }
 
     setupRecalculatedData(world) {
@@ -57,3 +104,15 @@ class NorthMapScript extends AllScripts {
         return true;
     }
 };
+
+function _rotatePipe(pipe) {
+    const nextRotation = {
+        "Pipe/pipe-v": "Pipe/pipe-h",
+        "Pipe/pipe-h": "Pipe/pipe-v",
+        "Pipe/pipe-ul": "Pipe/pipe-ur",
+        "Pipe/pipe-ur": "Pipe/pipe-dr",
+        "Pipe/pipe-dr": "Pipe/pipe-dl",
+        "Pipe/pipe-dl": "Pipe/pipe-ul",
+    };
+    pipe.image = nextRotation[pipe.image];
+}
